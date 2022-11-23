ac64fe44d7ddf0efa7a3a173de3c80d1b0094728
Mateus Reis <mateuskonige@gmail.com>
2022-11-23 15:28:26 -0300

    https

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -85,7 +85,7 @@ const showingNavigationDropdown = ref(false);
 
                         <svg
                           class="ml-2 -mr-0.5 h-4 w-4"
-                          xmlns="http://www.w3.org/2000/svg"
+                          xmlns="https://www.w3.org/2000/svg"
                           viewBox="0 0 20 20"
                           fill="currentColor"
                         >
